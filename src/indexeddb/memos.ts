17500ccf7b840ea4f137df53b6c7cc6797ab1140
phwork12@gmail.com <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-08-15 00:23:28 +0900

    pagination

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -6,6 +6,14 @@ export interface MemoRecord {
   text: string
 }
 
+const NUM_PER_PAGE: number = 10
+
+export const getMemoPageCount = async():Promise<number> => {
+  const totalCount = await memos.count()
+  const pageCount = Math.ceil(totalCount/NUM_PER_PAGE)
+  return pageCount > 0 ? pageCount : 1 
+}
+
 const database = new Dexie('markdown-editor')
 database.version(1).stores({memos: '&datetime'})
 const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
@@ -15,6 +23,11 @@ export const putMemo = async (title: string, text: string): Promise<void> => {
   await memos.put({ datetime, title, text })
 }
 
-export const getMemos = ():Promise<MemoRecord[]> => {
-  return memos.orderBy('datetime').reverse().toArray()
+export const getMemos = (page: number): Promise<MemoRecord[]> => {
+  const offset = (page - 1) * NUM_PER_PAGE
+  return memos.orderBy('datetime')
+              .reverse()
+              .offset(offset)
+              .limit(NUM_PER_PAGE)
+              .toArray()
 }
